3b2c0a66d48a79149748b98c0c01bba9ee37ce52
Jesus Daniel Castro <jesusdaniel.castros@gmail.com>
2026-01-11 12:17:10 -0500

    operador ternario

---
+++ sentencias/casa_de_los_espejos.py
@@ -0,0 +1,12 @@
+print("="*40)
+print("*"*5, "CASA DE LOS ESPEJOS", "*"*5)
+print("="*40)
+
+edad = int(input("Cual es tu edad?: "))
+miedo = input("Tienes miedo a la oscuridad(si/no)?: ")
+miedo = miedo.strip().lower() == "si"
+
+if not miedo and edad >= 10:
+    print("Puedes ingresar...")
+else:
+    print("No puedes ingresar...")
