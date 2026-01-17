d75e966163dae167f7fef25aef733bb7871a9033
Jezzoner <jesusdaniel.castros@gmail.com>
2026-01-17 15:58:32 -0500

    prueba

--- ciclos/script_reto1.py
+++ ciclos/script_reto1.py
@@ -1,5 +1,5 @@
 print("="*40)
-print("*"*5, "SCRIPT DE MANTENIMIENTO - RETO 1", "*"*5)
+print("*"*5, "SCRIPT DE MANTENIMIENTO - RETO 1 GEMINI", "*"*5)
 print("="*40)
 
 servidores = ["192.168.1.10", "192.168.1.11", "192.168.1.20", "192.168.1.50"]
