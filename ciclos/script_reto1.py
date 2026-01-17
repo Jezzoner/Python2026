c75a0134a723b231d8a577d32c3c0e2c8d21294c
Jezzoner <jesusdaniel.castros@gmail.com>
2026-01-16 23:34:18 -0500

    script reto gemini 1

---
+++ ciclos/script_reto1.py
@@ -0,0 +1,8 @@
+print("="*40)
+print("*"*5, "SCRIPT DE MANTENIMIENTO - RETO 1", "*"*5)
+print("="*40)
+
+servidores = ["192.168.1.10", "192.168.1.11", "192.168.1.20", "192.168.1.50"]
+
+for servidor in servidores:
+   print(servidor, end=', ')
