50f2b8b85ee21da303c84ec1924c654e4614d10a
Jesus Daniel Castro <jesusdaniel.castros@gmail.com>
2026-01-17 17:29:26 -0500

    Reto 1 completado

--- ciclos/script_reto1.py
+++ ciclos/script_reto1.py
@@ -5,8 +5,19 @@ print("="*40)
 servidores = ["192.168.1.10", "192.168.1.11", "192.168.1.20", "192.168.1.50"]
 
 for servidor in servidores:
-   print(servidor, end='\n')
-   if servidor in ("192.168.1.20"):
-      print(servidor, "¡Error de conexión!")
-
-#desde la computadora
+   intentos = 0
+   print("-", servidor, end=' ====>\n')
+   if servidor == "192.168.1.20":
+      conexion_exitosa = False
+      print("¡Error de conexión!")
+      while intentos < 3:
+         if conexion_exitosa:
+            print("¡Reconexión exitosa!")
+            break
+         intentos += 1
+         print(f"Reintentando conexión... (intento {intentos})")
+      else:
+         print("Abortando. Servidor caído.")
+   else:
+      conexion = True
+      print(f" Ping exitoso: {servidor} ONLINE")
