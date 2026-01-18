39a46a3ceae879225ed903b4a531ce768cd1a856
Jesus Daniel Castro <jesusdaniel.castros@gmail.com>
2026-01-18 16:11:51 -0500

    operaciones con set

---
+++ colecciones/operaciones_con_set.py
@@ -0,0 +1,41 @@
+print('*** Operaciones con Set ***')
+
+a = {1, 2, 3, 4}
+b = {3, 4, 5, 6}
+
+union = a | b
+print(f'Union a | b: {union}')
+
+interseccion = a & b
+print(f'Intersección a & b {interseccion}')
+
+diferencia = a - b
+print(f'Diferencia a - b {diferencia}')
+
+print('*** Lista de Suscriptores ***')
+
+suscriptores = {'luisa@mail.com', 'marcos@mail.com', 'elena@mail.com'}
+print(f'Lista de suscriptores inicial: {suscriptores}')
+
+# Verifica si un nuevo suscriptor ya está en la lista
+nuevo_suscriptor = 'karla@mail.com'
+if nuevo_suscriptor in suscriptores:
+   print(f'El nuevo suscriptor ya está en la lista {nuevo_suscriptor}')
+else:
+   suscriptores.add(nuevo_suscriptor)
+   print(f'El nuevo suscriptor se ha agregado a la lista {nuevo_suscriptor}')
+print(f'Lista de suscriptores: {suscriptores}')
+
+# Eliminamos un suscriptor
+suscriptor_eliminar = 'elena@mail.com'
+suscriptores.remove(suscriptor_eliminar)
+print(f'El suscriptor {suscriptor_eliminar} ha sido eliminado de la lista')
+print(f'Lista de suscriptores: {suscriptores}')
+
+# Verificamos la cantidad total de suscriptores
+print(f'Cantidad total suscriptores: {len(suscriptores)}')
+
+# Mostramos todos los suscriptores
+print(f'--- Lista de Suscriptores ---')
+for suscriptor in suscriptores:
+   print(f'- {suscriptor}')
