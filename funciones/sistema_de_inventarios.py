a732b3bae1a06aba5695db1a4adc107c72e3231b
Jezzoner <jesusdaniel.castros@gmail.com>
2026-01-22 17:52:33 -0500

    gestion de inventarios con funciones finalizado

--- funciones/sistema_de_inventarios.py
+++ funciones/sistema_de_inventarios.py
@@ -19,10 +19,12 @@ for i in range(cant_agregar):
       'cantidad':cantidad
       }
    inventario.append(producto)
+id = producto['id']
 
-print("\nIntentario inicial:",inventario)
+print("\nIntentario inicial:",inventario, "\n")
 
 def mostrar_inventario():
+   print()
    print("-"*3, "Inventario del almacen", "-"*3)
    for producto in inventario:
       print(f'''
@@ -33,17 +35,20 @@ def mostrar_inventario():
          ''')  
 
 def agregar_nuevo_producto():
-   print(f"\nProporciona los valores del producto {i}")
+   print(f"\nProporciona los valores del producto")
    nombre = input("Nombre: ")
    precio = float(input("Precio: "))
    cantidad = int(input("Cantidad: "))
+   global id
+   id += 1
    producto = {
-      'id':i,
+      'id': id,
       'nombre':nombre,
       'precio':precio,
       'cantidad':cantidad
       }
    inventario.append(producto)
+   print(f"\nProducto \"{producto['nombre']}\" agregado exitosamente.\n")
 
 def buscar_producto_por_id():
    busqueda_id = int(input("\nIngresa el ID del producto a buscar: "))
@@ -63,25 +68,22 @@ def buscar_producto_por_id():
    else:
       print(f"\nProducto con ID {busqueda_id} NO encontrado\n")
 
-def salida():
-   return True
-
-salida = False
-
-while not salida:
-
-   print('''--- Menu ---
+if __name__ == "__main__":
+   while True:
+      print('''--- Menu ---
       1. Mostrar inventario
       2. Agregar nuevo producto
       3. Buscar producto por ID
       4. Salir''')
-   opcion = int(input("Proporciona una opcion (1-4): "))
-   if opcion == 1:
-      print(mostrar_inventario())
-   elif opcion == 2:
-      agregar_nuevo_producto()
-   elif opcion == 3:
-      buscar_producto_por_id()
-   elif opcion == 4:
-      print("Saliendo del sistema...")
-      salida = True
+      opcion = int(input("Proporciona una opcion (1-4): "))
+      if opcion == 1:
+         print(mostrar_inventario())
+      elif opcion == 2:
+         agregar_nuevo_producto()
+      elif opcion == 3:
+         buscar_producto_por_id()
+      elif opcion == 4:
+         print("Saliendo del sistema...")
+         break
+      else:
+         print("Opcion invalida, proporciona una opcion valida")
