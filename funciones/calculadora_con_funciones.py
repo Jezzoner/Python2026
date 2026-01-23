456798457ed112ce6872d769be14a09ba1d6f7b2
Jezzoner <jesusdaniel.castros@gmail.com>
2026-01-22 23:29:19 -0500

    calculadora con funciones terminado

---
+++ funciones/calculadora_con_funciones.py
@@ -0,0 +1,55 @@
+print("="*40)
+print("*"*5, "Calculadora con Funciones", "*"*5)
+print("="*40)
+
+def mostrar_menu():
+   print('''\nOperaciones que puedes realizar:    
+      1. Suma
+      2. Resta
+      3. Multiplicacion
+      4. Division
+      5. Salir
+      ''')
+   return int(input("Escoge una opcion: "))
+
+def valores():
+   a = float(input("\nIngrese el primer valor: "))
+   b = float(input("Ingrese el segundo valor: "))
+   return (a, b)
+
+def suma():
+   a , b = valores()
+   resultado = a + b
+   print(f"\nEl resultado de la suma es: {resultado:.2f}")
+
+def resta():
+   a , b = valores()
+   resultado = a - b
+   print(f"\nEl resultado de la resta es:  {resultado:.2f}")
+
+def multiplicacion():
+   a , b = valores()
+   resultado = a * b
+   print(f"\nEl resultado de la multiplicacion es:  {resultado:.2f}")
+
+def division():
+   a , b = valores()
+   resultado = a / b
+   print(f"\nEl resultado de la division es:  {resultado:.2f}")
+
+if __name__ == "__main__":
+   while True:
+      opcion = mostrar_menu()
+      if opcion == 1:
+         suma()
+      elif opcion == 2:
+         resta()
+      elif opcion == 3:
+         multiplicacion()
+      elif opcion == 4:
+         division()
+      elif opcion == 5:
+         print("\nGracias por usar la calculadora")
+         break
+      else:
+         print("Opcion invalida, proporcione una opcion valida")
