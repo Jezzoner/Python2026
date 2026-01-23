4c546e73d6539f7fa1353175ad3f4e3f9772ba56
Jezzoner <jesusdaniel.castros@gmail.com>
2026-01-22 21:54:25 -0500

    maquina de snack terminado

--- funciones/maquina_snacks.py
+++ funciones/maquina_snacks.py
@@ -7,8 +7,8 @@ snacks = [
    {'id': 2, 'nombre': 'Refresco', 'precio': 50},
    {'id': 3, 'nombre': 'Sandwich', 'precio': 120}
 ]
-compras = []
-id = 1
+productos = []
+id = (snacks[-1].get('id'))
 
 #Opcion 1
 def mostrar_snacks():
@@ -21,36 +21,57 @@ def mostrar_snacks():
    Precio: ${snack.get('precio')}
    ''')  
 
-#Opcion 2
-def comprar_snacks(snack_a_comprar):
+def agregar_snack():
+   global id
+   id += 1
+   nombre = input("Nombre del Snack: ")
+   precio = int(input("Precio del Snack: "))
+   snack = {
+      'id':id,
+      'nombre':nombre,
+      'precio':precio
+   }
+   snacks.append(snack)
+   print(snacks)
+
+
+#Busqueda de Snack por id
+def busqueda_snack(id_buscar):
    for snack in snacks:
-      if snack.get('id') == snack_a_comprar:
-            print(f'\nSnack agregado --> ID: {snack.get('id')}, Nombre: {snack.get('nombre')}, Precio: ${snack.get('precio')}\n')
-            global id
-            snack = {
-               'id': id,
-               'nombre': snack.get('nombre'),
-               'precio': snack.get('precio')
-            }
-            compras.append(snack)
-            id += 1
-            return
-   print(f"\nSnack NO encontrado con ID: {snack_a_comprar}\n")
+      if snack.get('id') == id_buscar:
+         return snack
+   return None
+
+#Opcion 2
+def comprar_snacks():
+   mostrar_snacks()
+   snack_id = int(input("\nQue Snack quieres comprar (ID): "))
+   snack_encontrado = busqueda_snack(snack_id)
+   if snack_encontrado is not None:
+      productos.append(snack_encontrado)
+      print(f"\nSnack agregado: {snack_encontrado.get('nombre')}\n")   
+   else:  
+      print(f"\nSnack NO encontrado con ID: {snack_id}\n")
 
 #Opcion 3
 def mostrar_ticket():
    print()
-   print("-"*4, "Ticket de Venta", "-"*4,"\n")
+   print("*"*6, "Ticket de Venta", "*"*6,"\n")
    total = 0
-   for compra in compras:
-      print(f"- {compra.get('nombre')} ... ${compra.get('precio')}")
-      total = total + compra.get('precio')
-   print(f"\nTOTAL --> ${total}\n")
+   for producto in productos:
+      print(f"- {producto.get('nombre'):<15} ... ${producto.get('precio')}")
+      total += producto.get('precio')
+   print("-" * 30)
+   print(f"TOTAL A PAGAR:        ${total}")
+   print("-" * 30)
+   print()
 
+# --- main ---
 
 if __name__ == "__main__":
    while True:
       print('''Menu:
+         0. Agregar Snack
          1. Mostrar Snacks
          2. Comprar Snacks
          3. Mostrar Ticket
@@ -60,13 +81,13 @@ if __name__ == "__main__":
       if opcion == 1:
          mostrar_snacks()
       elif opcion == 2:
-         snack_a_comprar = int(input("\nQue Snack quieres comprar (ID): "))
-         comprar_snacks(snack_a_comprar)
+         comprar_snacks()
       elif opcion == 3:
-         print(compras)
          mostrar_ticket()
       elif opcion == 4:
          print("\nSaliendo del sistema...")
          break
+      elif opcion == 0:
+         agregar_snack()
       else:
          print("Opcion invalida, proporcione una opcion valida")
