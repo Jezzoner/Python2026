9cc09c2c0e07e1c94b1636be616ba581768fd1a7
Jesus Daniel Castro <jesusdaniel.castros@gmail.com>
2026-02-24 21:40:36 -0500

    maquina de snacks terminado

--- manejo/maquina_snacks_proyecto/snack.py
+++ manejo/maquina_snacks_proyecto/snack.py
@@ -2,15 +2,15 @@ class Snack:
 
    contador = 0
 
-   def __init__(self, nombre='',precio=0.0):
+   def __init__(self, nombre=str,precio=float):
       Snack.contador += 1
       self.id_snack = Snack.contador
       self.nombre = nombre
       self.precio = precio
 
    def __str__(self):
-      return (f'snack id: {self.id_snack}, nombre: {self.nombre}, '
-      f'precio: {self.precio}')
+      return (f'Snack -> ID: {self.id_snack}, Nombre: {self.nombre}, '
+      f'Precio: ${self.precio:.2f}')
 
    def escribir_snack(self):
       return f"{self.id_snack}, {self.nombre}, {self.precio}"
